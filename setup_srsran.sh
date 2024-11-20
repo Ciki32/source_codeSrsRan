8748dffb80c1c71f0ee75220f95e696dae2b831c
Maulana Deski <145884495+Ciki32@users.noreply.github.com>
2024-11-20 22:21:24 +0700

    Add files via upload

---
+++ setup_srsran.sh
@@ -0,0 +1,17 @@
+
+sudo chmod +x GUISrsRAN.sh
+echo "OK"
+
+sudo chmod +x run_srsran.sh
+echo "OK"
+
+sudo chmod +x stop_srsran.sh
+echo "OK"
+
+echo "Setup SrsRAN Selesai!"
+
+echo "==============================="
+echo "jalankan program berikut"
+echo "Web GUI SrsRAN: ./GUISrsRAN.sh"
+echo "Running SrsRAN: ./run_srsran.sh"
+echo "================================"
