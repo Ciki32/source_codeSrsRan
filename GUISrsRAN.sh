70c53bd7a1cc5553b6a0415009674f44a565a030
Maulana Deski <145884495+Ciki32@users.noreply.github.com>
2024-11-23 10:50:05 +0700

    Update GUISrsRAN.sh

--- GUISrsRAN.sh
+++ GUISrsRAN.sh
@@ -1,7 +1,6 @@
 
 sudo chmod -R 777 /var/www/html
 sleep 1
-echo "OK"
 
 sudo cp epc.php error_ignore.php index.php style.css user.php /var/www/html
 sleep 1
@@ -9,7 +8,6 @@ sleep 1
 sudo chmod -R 777 /etc/srsran
 sudo chmod -R 777 /var/www/html
 sleep 1
-echo "OK"
 
 sudo systemctl start apache2
 echo "Web GUI SrsRAN Berhasil diaktfifasi !"
